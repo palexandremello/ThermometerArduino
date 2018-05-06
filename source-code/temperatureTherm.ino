d93e9a427524298b1f000fd6e5bc620c1bb4b8fb
Paulo Alexandre <palexandremello@gmail.com>
2018-05-06 18:02:14 -0300

    new code

--- source-code/temperatureTherm.ino
+++ source-code/temperatureTherm.ino
@@ -1,33 +1,57 @@
-#define vRef  5.015
-#define tRef  25+273.15
-#define resistorRef  22000
-#define rRef 20000
-#define pinResistor A0
+#define vcc 5.00
+#define t25 273.15 + 25.00
+#define R0 22000.00
+#define R25 20000.00
+#define pinRF A0
 #define redPin 8
-#define betaFactor 3950 // Fator necessario para equation Factor Beta (unidade em Kelvin)
+#define yellowPin 9
+#define bluePin 10
+#define beta 3950.00
+
 #include <math.h>
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(redPin,OUTPUT);
 
+Serial.begin(9600);
+
+pinMode(redPin,OUTPUT);
+pinMode(yellowPin,OUTPUT);
+pinMode(bluePin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue  =  analogRead(pinResistor);
-  double vTherm =  (vRef*sensorValue) / 1024.000;
-  double reTherm   =  (vRef/vTherm - 1.0) * resistorRef;
-  double temperature = ( betaFactor / log(reTherm / (rRef * exp(-betaFactor/(tRef) ) ) ) ) - 273.15;
-  Serial.print(vTherm);
-  Serial.print(" ");
-  Serial.print(reTherm);
-  Serial.print(" ");
-  Serial.print("Temp = ");
-  Serial.println(temperature);
-
-  if ( temperature > 22 & temperature < 25) digitalWrite(redPin,HIGH);
-
-  delay(1000);
+    int sensor = analogRead(pinrf);
+    double vt = (5*sensor)/1024.00;
+    double rterm = ((vrf/vt)-1)*rrf;
+    double den = log(rterm/(r25*exp(-beta/(trf))));
+    double temp = beta/den -273.15;
+    Serial.println(temp);
+   
+    if (temp < 0 || temp => 26 ) {
+      digitalWrite(redPin,HIGH);
+      delay(100);
+      digitalWrite(redPin,LOW);
+      delay(100);
+      digitalWrite(yellowPin,LOW);
+      digitalWrite(bluePin,LOW);
+      Serial.println("Alerta Vermelho");
+   
+    } else if (temp > 20 & temp < 25) {
+      digitalWrite(redPin,LOW);
+      digitalWrite(yellowPin,HIGH);
+      digitalWrite(bluePin,LOW);
+      Serial.println("Alerta Amarelo");
+     
+    } else if (temp > 0 & temp < 19) {
+      digitalWrite(redPin,LOW);
+      digitalWrite(yellowPin,LOW);
+      digitalWrite(bluePin,HIGH);
+      Serial.println("Alerta Azul");
+     
+    }
+   
+    delay(1000);
+   
 
 }
